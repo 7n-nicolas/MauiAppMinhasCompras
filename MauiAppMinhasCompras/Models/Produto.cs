f05166184ee1b60ec3094772db8c5bbe982e2d9b
7nnic <7nnic@DESKTOP-SS40T74>
2026-09-05 18:59:40 -0300

    restringindo valores negativos na inserção de dados

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,10 +1,13 @@
-﻿using SQLite;
+﻿using System;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
         string _descricao;
+        double _quantidade;
+        double _preco;
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
@@ -13,15 +16,37 @@ namespace MauiAppMinhasCompras.Models
             get => _descricao;
             set
             {
-                if (value == null)
+                if (string.IsNullOrWhiteSpace(value))
                 {
                     throw new Exception("A descrição do produto não pode ser vazia.");
                 }
                 _descricao = value;
             }
         }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("A quantidade do produto deve ser um valor positivo.");
+                }
+                _quantidade = value;
+            }
+        }
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("O preço do produto deve ser um valor positivo.");
+                }
+                _preco = value;
+            }
+        }
 
         public double Total { get => Quantidade * Preco; }
     }
