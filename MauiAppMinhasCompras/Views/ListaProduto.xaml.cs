64f9a03e7b9ed7b0dfb1ac4a528b9c96bed4355d
7nnic <7nnic@DESKTOP-SS40T74>
2026-09-11 21:37:36 -0300

    adicionado R$, recarga dinamica e atualização front end

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -46,6 +46,8 @@ public partial class ListaProduto : ContentPage
         {
             string q = e.NewTextValue;
 
+            lst_produtos.IsRefreshing = true;
+
             lista.Clear();
 
             List<Produto> tmp = await App.Db.Search(q);
@@ -55,6 +57,9 @@ public partial class ListaProduto : ContentPage
         catch (Exception ex)
         {
             await DisplayAlert("Erro", ex.Message, "OK");
+        } finally
+        {
+            lst_produtos.IsRefreshing = false;
         }
     }
 
@@ -105,6 +110,25 @@ public partial class ListaProduto : ContentPage
         catch (Exception ex)
         {
             DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
+
+    private async void lst_produtos_Refreshing(object sender, EventArgs e)
+    {
+        try
+        {
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
+        } finally
+        {
+            lst_produtos.IsRefreshing = false;
         }
     }
 }
